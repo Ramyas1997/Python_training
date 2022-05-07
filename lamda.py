65a22e331fa46f99d165e123b90d5f364fbf71e1
Ramyas1997 <103407221+Ramyas1997@users.noreply.github.com>
2022-05-07 15:37:06 +0530

    commit

---
+++ lamda.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+sum = lambda a,b: a+b
+minus = lambda a,b: a-b
+
+print(add(10,20))
+print(sub(100,50))
+print(sum(1000,2000))
+print(minus(1000,200))
